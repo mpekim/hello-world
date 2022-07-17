f05f7abaf8ebe9021d56216d4b22d2acd6ed0877
mpekim_wsu <mpmorley@worcester.edu>
2022-07-17 08:08:45 -0400

    Adding code to HTML, CSS and JS to update IDE. Console found and tests show code working as intended.

--- main.js
+++ main.js
@@ -2,4 +2,21 @@ function helloWorld()
 {
     alert("Hello World.");
     alert("Due to the console not running as intended, this is how the IDE will present output.");
+
+    var dog = {
+        legs: 4,
+        name: "Sparky",
+        ishappy: true
+    }
+
+    alert("Dog name: " + dog.name);
+
+    dog.name = "Fido";
+
+    alert("New Dog name: " + dog.name);
+
+    console.log("Testing...");
+    console.log("Good. Glad to see that the console can be found using F12.");
+
+    console.log(dog);
 }
