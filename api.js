bab8c11a66c271c3260ab2cc91957158987a381a
mpekim_wsu <mpmorley@worcester.edu>
2022-11-09 09:11:49 -0500

    Adding minor implementation to API stubs.

--- api.js
+++ api.js
@@ -1,9 +1,18 @@
 function getItem()
 {
-
+    // Function is meant to emulate the
+    // "GET" endpoint of an API.
+    console.log("Accessing GET...");
 }
 
 function postItem()
 {
-    
+    // Function is meant to emulate the
+    // "POST" endpoint of an API.
+    console.log("Accessing POST...");
+}
+
+function deleteItem()
+{
+    console.log("Accessing DELETE...");
 }
