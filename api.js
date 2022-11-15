b228f13ede6ca6e8e450ae564a707f225c5f15bb
mpekim_wsu <mpmorley@worcester.edu>
2022-11-15 00:25:50 -0500

    Additional code for Item class.

--- api.js
+++ api.js
@@ -3,6 +3,14 @@ class Item{
         this.name = name;
         this.id = id;
     }
+
+    getName(){
+        return this.name;
+    }
+
+    getId(){
+        return this.id;
+    }
     // Stub for Item class.
 }
 
