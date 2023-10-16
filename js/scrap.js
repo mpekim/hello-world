3dcb245606ae8a37cadc006731880b0c40416590
mpekim_wsu <mpmorley@worcester.edu>
2023-10-16 06:18:10 -0400

    Moving commented-out code and other unrelated functions to scrap file.

--- js/scrap.js
+++ js/scrap.js
@@ -1,3 +1,94 @@
 // Scrap JS code file.
 // This file is meant to hold test/junk code
 // That is being cleaned up from the other files.
+/*
+alert("Dog name: " + dog.name);
+
+dog.name = "Fido";
+
+alert("New Dog name: " + dog.name);
+
+
+//console.log("Testing...");
+//console.log("Good. Glad to see that the console can be found using F12.");
+
+console.log(dog);
+document.write("Let's see if this shows up on-screen.");
+document.write(dog);
+*/
+
+// This is all well and good, but let's work on the class practice.
+
+/*var fido = new Dog(4, "Fido", true);
+
+alert("Fido's leg count: " + fido.getLegs());
+fido.setLegs(16);
+alert("new leg count: " + fido.getLegs());
+fido.present();
+
+var defaultDog = new Dog();
+defaultDog.present(defaultDog);*/
+// Commenting out code due to changes in Dog class.
+
+/*var message = "hello";
+console.log("Message before shift: " + message);
+message = changeString(message, 1);
+console.log("Message after shift: " + message);*/
+// Comenting out code until messageShift() function works.
+
+// All test code will be "commented out" and "put back in" as needed.
+
+//console.log(factorial(3));
+
+// Functions below are meant to be implemented as Object methods
+// at some point.
+//-----------------------------------------------------------------------------
+/*
+// Pre-Condition: This function accepts a "dog" parameter.
+// Post-Condition: The legs value of the "dog" object
+// will be returned.
+function getLegs(dog)
+{
+return dog.legs;
+}
+
+// Pre-Condition: This function accepts a "dog" parameter.
+// Post-Condition: The name value of the "dog" object
+// will be returned.
+function getName(dog)
+{
+return dog.name;
+}
+
+function getHappy(dog)
+{
+return dog.isHappy;
+}
+
+// Pre-Condition: This function accepts a "newLegs" parameter.
+// Post-Condition: The legs value of the "dog" object
+// will be set to the input value.
+function setLegs(newLegs)
+{
+dog.legs = newLegs;
+}
+
+// Pre-Condition: This function accepts a "newName" parameter.
+// Post-Condition: The name value of the "dog" object will
+// be set to the input value.
+function setName(newName)
+{
+dog.name = newName;
+}
+
+// Pre-Condition: This function accepts a "newHappy" parameter.
+// Post-Condition: The isHappy value of the "dog" object
+// will be set to the input value.
+function setHappy(newHappy)
+{
+dog.isHappy = newHappy;
+}*/ // Code commented out due to removal of Dog class.
+// End of the functions for the Dog object.
+
+let message = (input) => console.log("Hello " + input);
+// ES6 Arrow functions. Basic practice.
