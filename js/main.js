3c478a1fa8b2732454d469f81a28de78a8116fc6
mpekim_wsu <mpmorley@worcester.edu>
2023-10-20 00:55:10 -0400

    Making changes to main.js file (mainly moving functions to scrap file).

--- js/main.js
+++ js/main.js
@@ -131,96 +131,7 @@ function helloWorld()
     d1.presentDog();
     // Code works, but the result is the same.
     // Also, I don't understand why concat doesn't work.
-    /*
-
-    alert("Dog name: " + dog.name);
-
-    dog.name = "Fido";
-
-    alert("New Dog name: " + dog.name);
-
-
-    //console.log("Testing...");
-    //console.log("Good. Glad to see that the console can be found using F12.");
-
-    console.log(dog);
-    document.write("Let's see if this shows up on-screen.");
-    document.write(dog);
-    */
-
-    // This is all well and good, but let's work on the class practice.
-
-    /*var fido = new Dog(4, "Fido", true);
-
-    alert("Fido's leg count: " + fido.getLegs());
-    fido.setLegs(16);
-    alert("new leg count: " + fido.getLegs());
-    fido.present();
-
-    var defaultDog = new Dog();
-    defaultDog.present(defaultDog);*/
-    // Commenting out code due to changes in Dog class.
-
-    /*var message = "hello";
-    console.log("Message before shift: " + message);
-    message = changeString(message, 1);
-    console.log("Message after shift: " + message);*/
-    // Comenting out code until messageShift() function works.
-
-    // All test code will be "commented out" and "put back in" as needed.
-
-    //console.log(factorial(3));
-    
 }
-// Functions below are meant to be implemented as Object methods
-// at some point.
-//-----------------------------------------------------------------------------
-/*
-// Pre-Condition: This function accepts a "dog" parameter.
-// Post-Condition: The legs value of the "dog" object
-// will be returned.
-function getLegs(dog)
-{
-    return dog.legs;
-}
-
-// Pre-Condition: This function accepts a "dog" parameter.
-// Post-Condition: The name value of the "dog" object
-// will be returned.
-function getName(dog)
-{
-    return dog.name;
-}
-
-function getHappy(dog)
-{
-    return dog.isHappy;
-}
-
-// Pre-Condition: This function accepts a "newLegs" parameter.
-// Post-Condition: The legs value of the "dog" object
-// will be set to the input value.
-function setLegs(newLegs)
-{
-    dog.legs = newLegs;
-}
-
-// Pre-Condition: This function accepts a "newName" parameter.
-// Post-Condition: The name value of the "dog" object will
-// be set to the input value.
-function setName(newName)
-{
-    dog.name = newName;
-}
-
-// Pre-Condition: This function accepts a "newHappy" parameter.
-// Post-Condition: The isHappy value of the "dog" object
-// will be set to the input value.
-function setHappy(newHappy)
-{
-    dog.isHappy = newHappy;
-}*/ // Code commented out due to removal of Dog class.
-// End of the functions for the Dog object.
 
 //--------------------------------------------------------------------------------------
 // Pre-Condition: This function accepts an argument, "input", for the limit
@@ -279,6 +190,3 @@ function convertToJSobject(obj){
     JSON.parse(obj);
     return obj;
 }
-
-let message = (input) => console.log("Hello " + input);
-// ES6 Arrow functions. Basic practice.
